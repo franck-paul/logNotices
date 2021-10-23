8cec15f44196dcd51622c1d64422c18779958dae
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:19 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,18 +10,23 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Store notices in log table",                      // Name
-    "Store all or error only notices in the database", // Description
-    "Franck Paul and contributors",                    // Author
+    'Store notices in log table',                      // Name
+    'Store all or error only notices in the database', // Description
+    'Franck Paul and contributors',                    // Author
     '0.1',                                             // Version
     [
         'requires'    => [['core', '2.17']],               // Dependencies
         'permissions' => 'usage,contentadmin',             // Permissions
         'type'        => 'plugin',                         // Type
-        'settings'    => ['blog' => '#params.logNotices'] // Settings
+        'settings'    => ['blog' => '#params.logNotices'], // Settings
+
+        'details'    => 'https://open-time.net/?q=logNotices',       // Details URL
+        'support'    => 'https://github.com/franck-paul/logNotices', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/logNotices/main/dcstore.xml'
     ]
 );
