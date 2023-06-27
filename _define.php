a18e6e31b67573b25fa63ad69886541b8de23e94
franck <carnet.franck.paul@gmail.com>
2023-06-27 09:38:33 +0200

    Release 2.1.1

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Store notices in log table',
     'Store all or error only notices in the database',
     'Franck Paul and contributors',
-    '2.1',
+    '2.1.1',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
