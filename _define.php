36d6828d4ee8be0ef63d0d892584adb05a35722c
franck <carnet.franck.paul@gmail.com>
2025-09-22 10:30:00 +0200

    New release 6.1

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'logNotices',
     'Store all or error only notices in the database',
     'Franck Paul and contributors',
-    '6.0',
+    '6.1',
     [
-        'date'        => '2025-09-07T15:58:06+0200',
+        'date'        => '2025-09-22T10:30:00+0200',
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
